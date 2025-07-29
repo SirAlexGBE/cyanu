715ec8f300497442717942601150683b8bcc6d06
SirAlexGBE <Bindasjake14@Gmail.com>
2025-07-29 21:14:30 +0545

    sd

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Navbar from "./Components/Layout/Navbar";
 import HeroSection from "./Components/UI/Hero";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <HeroSection />
     </>
   );
