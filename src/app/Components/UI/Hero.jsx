9805ee1192335c5dbbf32d7ae1f58ba6c90f50ba
SirAlexGBE <Bindasjake14@Gmail.com>
2025-07-30 13:43:53 +0545

    colour correction

--- src/app/Components/UI/Hero.jsx
+++ src/app/Components/UI/Hero.jsx
@@ -138,7 +138,7 @@ const HeroSection = () => {
         <div className="relative z-10 p-4">
           <div className="mx-auto w-full text-center mb-16">
             <div className="flex items-center justify-center mb-6">
-              <Sparkles className="w-5 h-5 text-[#EADD8F] mr-2 animate-pulse" />
+              <Sparkles className="w-5 h-5 text-[#bcb584] mr-2 animate-pulse" />
               <span className="text-[#EADD8F] text-sm sm:text-base font-medium tracking-wider uppercase">Next-Generation Software Solutions</span>
               <Sparkles className="w-5 h-5 text-[#EADD8F] ml-2 animate-pulse" />
             </div>
@@ -162,7 +162,7 @@ const HeroSection = () => {
                 <div className="relative p-2 flex items-center justify-center text-center flex-col backdrop-blur-sm border border-gray-800 rounded-2xl hover:border-[#EADD8F]/50 transition-all duration-500 hover:transform hover:scale-105 h-48">
                   <div className="absolute inset-0 bg-transparent  transition-opacity duration-500 rounded-2xl" />
                   <div className="relative z-10 flex flex-col items-center justify-center space-y-2">
-                    <Icon className="w-8 h-8 text-[#EADD8F] group-hover:rotate-12 transition-transform duration-500" aria-label={label} />
+                    <Icon className="w-8 h-8 text-[#cabb67] group-hover:rotate-12 transition-transform duration-500" aria-label={label} />
                     <h3 className="text-white font-bold text-xl">{label}</h3>
                     <p className="text-gray-300 text-sm">{description}</p>
                   </div>
