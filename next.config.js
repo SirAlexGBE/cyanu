b8f9e02e7794cd98bd74d81d3abeb831cc5096c4
SirAlexGBE <Bindasjake14@Gmail.com>
2025-07-31 11:48:01 +0545

    next config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,26 @@
 const path = require("path");
+// const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
 
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, {dev}) => {
+    // Alias @ to src
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
+
+    // Disable symlinks resolving for some reason (keep if you need it)
     config.resolve.symlinks = false;
+
+    // Add babel-loader rule for js/tsx/jsx/ts files (usually Next.js already has this, but keep if needed)
     config.module.rules.push({
       test: /\.(js|jsx|ts|tsx)$/,
       loader: "babel-loader",
       exclude: /node_modules/,
     });
 
-    // This helps error on mismatched casing locally
-    config.resolve.plugins = config.resolve.plugins || [];
-    const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
-    config.resolve.plugins.push(new CaseSensitivePathsPlugin());
+    // // Add CaseSensitivePathsPlugin only in development to avoid build errors on Vercel
+    // if (dev) {
+    //   config.resolve.plugins = config.resolve.plugins || [];
+    //   config.resolve.plugins.push(new CaseSensitivePathsPlugin());
+    // }
 
     return config;
   },
