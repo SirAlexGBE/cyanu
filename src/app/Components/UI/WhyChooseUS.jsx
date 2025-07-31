010068b25765d0074aac38603179fbecc41dae80
SirAlexGBE <Bindasjake14@Gmail.com>
2025-07-31 11:26:53 +0545

    why

--- src/app/Components/UI/WhyChooseUS.jsx
+++ src/app/Components/UI/WhyChooseUS.jsx
@@ -1,7 +1,7 @@
 "use client";
 import {motion} from "framer-motion";
 import Image from "next/image";
-import {team, scale, security, delivery, innovation, satisfaction} from "@/Assets/images";
+import {team, scale, security, delivery, innovation, satisfaction} from "@/assets/images";
 
 const features = [
   {
