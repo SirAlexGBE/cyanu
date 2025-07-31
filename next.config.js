e2b483581e4bd60116befea4efb69e186f0b5e4b
SirAlexGBE <Bindasjake14@Gmail.com>
2025-07-31 11:28:55 +0545

    khai k k ho

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 const path = require("path");
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    config.resolve.symlinks = false;
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      loader: "babel-loader",
+      exclude: /node_modules/,
+    });
+
+    // This helps error on mismatched casing locally
+    config.resolve.plugins = config.resolve.plugins || [];
+    const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+    config.resolve.plugins.push(new CaseSensitivePathsPlugin());
+
     return config;
   },
 };
