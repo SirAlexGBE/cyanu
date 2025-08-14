e5ac7d9b2dd3a9be780a0129ac85914d77d9e86a
SirAlexGBE <Bindasjake14@Gmail.com>
2025-08-14 16:44:00 +0545

    hero made

--- src/app/(users)/services/page.js
+++ src/app/(users)/services/page.js
@@ -1,5 +1,11 @@
+import HeroSection from "@/app/Components/UI/Services/hero";
+import ServiceHero from "@/app/Components/UI/Services/hero";
 import React from "react";
 
 export default function ServicesPage() {
-  return <>Hi</>;
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
 }
