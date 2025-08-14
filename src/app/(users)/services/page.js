518077110d55862a162ac44a802dc4307dfb0c92
SirAlexGBE <Bindasjake14@Gmail.com>
2025-08-14 17:03:44 +0545

    services card made

--- src/app/(users)/services/page.js
+++ src/app/(users)/services/page.js
@@ -1,11 +1,13 @@
 import HeroSection from "@/app/Components/UI/Services/hero";
 import ServiceHero from "@/app/Components/UI/Services/hero";
+import ServiceCards from "@/app/Components/UI/Services/Services";
 import React from "react";
 
 export default function ServicesPage() {
   return (
     <>
       <HeroSection />
+      <ServiceCards />
     </>
   );
 }
